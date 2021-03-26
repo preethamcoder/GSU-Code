ababeb4799f06202961e1910a6b9a81b0a3d7579
Preetham <sthelluri1@student.gsu.edu>
2021-03-25 20:13:21 -0400

    Added splitTime.c program

---
+++ CSC3320/C Programs/splitTime.c
@@ -0,0 +1,23 @@
+#include<stdio.h> 
+// Write the declaration of function split_time 
+void split_time(long total_sec, int *hr, int *min, int  *sec);
+int main(){ 
+int n, hr, min, sec; 
+printf("Enter seconds: "); 
+scanf("%d",&n); 
+/* Write the statement to call split_time */ 
+split_time(n, &hr, &min, &sec);
+printf("Converted format: %d hour %d mins %d secs", hr, min, sec); 
+return 0; 
+} 
+void split_time(long total_sec, int *hr, int *min, int *sec){ /* Write the statements to calculate hr, min and sec  */ 
+	*sec = 0;
+	*min = 0;
+	*hr = 0;
+	*sec = total_sec % 60;
+	total_sec = total_sec / 60;
+	*min = total_sec % 60;
+	total_sec = total_sec / 60;
+	*hr = total_sec % 60;
+	total_sec = total_sec / 60;
+}
