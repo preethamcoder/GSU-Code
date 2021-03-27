64c2ee642abed61a44a108ae317e1973a95ce07d
Preetham Thelluri <46249739+preethampython101@users.noreply.github.com>
2021-03-27 19:50:07 -0400

    Update splitTime.c

--- CSC3320/C Programs/splitTime.c
+++ CSC3320/C Programs/splitTime.c
@@ -1,14 +1,12 @@
 #include<stdio.h> 
-// Write the declaration of function split_time 
 void split_time(long total_sec, int *hr, int *min, int  *sec);
 int main(){ 
-int n, hr, min, sec; 
-printf("Enter seconds: "); 
-scanf("%d",&n); 
-/* Write the statement to call split_time */ 
-split_time(n, &hr, &min, &sec);
-printf("Converted format: %d hour %d mins %d secs", hr, min, sec); 
-return 0; 
+	int n, hr, min, sec; 
+	printf("Enter seconds: "); 
+	scanf("%d",&n); 
+	split_time(n, &hr, &min, &sec);
+	printf("Converted format: %d hour %d mins %d secs", hr, min, sec); 
+	return 0; 
 } 
 void split_time(long total_sec, int *hr, int *min, int *sec){ /* Write the statements to calculate hr, min and sec  */ 
 	*sec = 0;
