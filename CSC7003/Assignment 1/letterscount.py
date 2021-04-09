90e759c1564b2e4bf3d07c888aa72e2fd8948a18
Preetham Thelluri <46249739+preethampython101@users.noreply.github.com>
2021-04-08 20:20:29 -0400

    Update letterscount.py

--- CSC7003/Assignment 1/letterscount.py
+++ CSC7003/Assignment 1/letterscount.py
@@ -4,7 +4,6 @@ import string, sys
 # it then reads the file into a string variable
 # then proceeds to process the string for the count of letters
 # answer should be returned in a list of 26 numbers
-
 def draw(counts):
     lett = list(string.ascii_lowercase)
     xaxis = range(len(lett))
@@ -17,8 +16,6 @@ def draw(counts):
     for a,b in zip(xaxis, counts):
         plt.text(a, b+8, str(b), horizontalalignment='center', verticalalignment='center')
     plt.show()
-
-    
 #main()
 def get_count(fname):
     #print("In function")
@@ -30,7 +27,6 @@ def get_count(fname):
         for i in letters:
             numbers.append(data.count(i))
         return numbers
-
 def main():
     counts = get_count(sys.argv[1])
     draw(counts)
