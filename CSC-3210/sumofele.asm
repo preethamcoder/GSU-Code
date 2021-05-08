4fe73edc1b6afc41df00a7b669d4d229d326d98a
Preetham Thelluri <46249739+preethampython101@users.noreply.github.com>
2021-05-08 10:34:39 -0400

    Update sumofele.asm

--- CSC-3210/sumofele.asm
+++ CSC-3210/sumofele.asm
@@ -6,27 +6,18 @@ ExitProcess proto, dwExitCode:dword
 array_list WORD 10, 11, 13, 18, 21, 23, 24, 17, 45
 sizeOAr = ($-array_list)/(TYPE array_list)
 sum WORD 0
-ind WORD 0
 .code
 main PROC
-mov edx, OFFSET sum
 mov esi, 0
+mov esi, OFFSET array_list
 mov ecx, sizeOAr
 mov eax, 0
-mov ebx, 0
+mov ebx, WORD
 mov ax, sum
-mov bx, ind
 L1:
-	cmp esi, ecx
-	jl L2
-	jmp L4
-L2:
-	add ax, array_list[esi*4]
-L3:
-	inc esi
-	jmp L1
-L4:
-	mov sum, ax
+	add ax, [esi]
+	add esi, ebx
+LOOP L1
 invoke ExitProcess,0
 main endp
 end main
