f34077de357f88c867ad13b23e5ff2ad7dea4075
Preetham Thelluri <46249739+preethamcoder@users.noreply.github.com>
2021-11-03 21:28:26 -0400

    This is the conventional way

---
+++ CSC4520/reverse_integer_mod_10.py
@@ -0,0 +1,16 @@
+def reverse_digit(inp):
+    if(inp == 0):
+         return 0
+    else:
+        rev = 0
+        while(inp != 0):
+            rev += inp % 10
+            if(inp > 9):
+                rev *= 10
+            inp = inp // 10
+        return rev
+
+if __name__ == '__main__':
+	num = int(input("Enter the number you want to reverse: "))
+	reverse = reverse_digit(num)
+	print(reverse)
