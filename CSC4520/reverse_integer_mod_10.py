1aad413edf2ee4446c8bb621c194b288d9ce0cdf
Preetham Thelluri <46249739+preethamcoder@users.noreply.github.com>
2021-11-03 21:38:46 -0400

    Added negative numbers too

--- CSC4520/reverse_integer_mod_10.py
+++ CSC4520/reverse_integer_mod_10.py
@@ -3,12 +3,20 @@ def reverse_digit(inp):
          return 0
     else:
         rev = 0
-        while(inp != 0):
-            rev += inp % 10
-            if(inp > 9):
-                rev *= 10
-            inp = inp // 10
-        return rev
+        if(inp < 0):
+            while(inp != 0):
+                rev += inp % 10
+                if(inp > 9):
+                    rev *= 10
+                inp = inp // 10
+            return (rev * -1)
+        else:
+            while(inp != 0):
+                rev += inp % 10
+                if(inp > 9):
+                    rev *= 10
+                inp = inp // 10
+            return rev
 
 if __name__ == '__main__':
 	num = int(input("Enter the number you want to reverse: "))
