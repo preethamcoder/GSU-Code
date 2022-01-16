919a55a7fe7d5f2748eee22ee513806af7f0ece0
Preetham Thelluri <46249739+preethamcoder@users.noreply.github.com>
2022-01-16 00:15:04 -0500

    GIt commands

---
+++ CSC-4350/hw1/run.sh
@@ -0,0 +1,8 @@
+#Assuming that you have the files and folders ready, execute this shell script
+git init
+git status
+git add *
+git status
+git commit -m "Initial commit"
+git remote add <your-repo-url>
+git push origin master
