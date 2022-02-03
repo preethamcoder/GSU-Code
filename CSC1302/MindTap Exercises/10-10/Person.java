17f761f04a831597d9afa11b4a8c6828e37c4e41
Preetham Thelluri <46249739+preethamcoder@users.noreply.github.com>
2022-02-02 23:02:49 -0500

    FInal

--- CSC1302/MindTap Exercises/10-10/Person.java
+++ CSC1302/MindTap Exercises/10-10/Person.java
@@ -9,14 +9,19 @@ public class Person
     Scanner input = new Scanner(System.in);
     public void setData()
     {
+        System.out.println("Enter first name: ");
         firstName = input.next();
         System.out.println(firstName);
+        System.out.println("Enter last name: ");
         lastName = input.next();
         System.out.println(lastName);
+        System.out.println("Enter address: ");
         address = input.next();
         System.out.println(address);
+        System.out.println("Enter zipcode: ");
         zip = input.next();
         System.out.println(zip);
+        System.out.println("Enter phone number: ");
         phone = input.next();
     }
     public void display()
