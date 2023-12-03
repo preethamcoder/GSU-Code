9cbc8b9a936b0c8ab061b2ebc6200ba0b410512c
Preetham <sthelluri1@student.gsu.edu>
2023-12-02 19:45:15 -0500

    Added numbers in increasing and decreasing order

--- Other/recursion.py
+++ Other/recursion.py
@@ -3,8 +3,26 @@ def check_sorted(arr):
         return True
     return arr[0] <= arr[1] and check_sorted(arr[1:])
 
+def get_1_to_N_inc(N, s, r):
+    r.append(s)
+    if s == N:
+        return 
+    s += 1
+    get_1_to_N_inc(N, s, r)
+    return r
+
+def get_N_to_1_dec(N, r):
+    r.append(N)
+    if N == 1:
+        return
+    N -= 1
+    get_N_to_1_dec(N, r)
+    return r
+
 if __name__ == '__main__':
     res = check_sorted([1, 2, 3, 4, 5, 6])
     print(res)
     r2 = check_sorted([1, 2, 9, 6])
     print(r2)
+    print(get_1_to_N_inc(100, 1, []))
+    print(get_N_to_1_dec(100, []))
