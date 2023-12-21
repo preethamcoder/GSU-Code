cd6ca90b098d83ed61b53a0870b9ed19aeb73ce9
Preetham Thelluri <46249739+preethamcoder@users.noreply.github.com>
2023-12-20 21:56:03 -0500

    Update Queue.py

--- CSC4520/Queues/Queue.py
+++ CSC4520/Queues/Queue.py
@@ -2,20 +2,15 @@ class Queue:
   
 	def __init__(self):
 		self.queue = []
-	
-  def enqueue(self, elem):
+  	def enqueue(self, elem):
 		self.queue.append(elem)
-	
-  def dequeue(self):
+  	def dequeue(self):
 		if(len(self.queue == 0)):
-      return "Queue is empty!"
-    return(self.queue.pop(0))
-
-  def peek(self):
+      			return "Queue is empty!"
+    		return(self.queue.pop(0))
+  	def peek(self):
 		return(self.queue[0])
-	
-  def elems(self):
+  	def elems(self):
 		return(self.queue)
-	
-  def length(self):
+  	def length(self):
 		return(len(self.queue))
